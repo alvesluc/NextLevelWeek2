d41e90f71d86b4eca94bdb173f1091a6fad1af7b
Lucas <l.alves001@gmail.com>
2020-08-09 22:31:48 -0300

    Day - Finished

---
+++ mobile/src/components/PageHeader/index.tsx
@@ -0,0 +1,44 @@
+import React, { ReactNode } from 'react';
+import { View, Image, Text } from 'react-native';
+import { BorderlessButton } from "react-native-gesture-handler";
+
+import backIcon from '../../assets/images/icons/back.png';
+import logoImage from '../../assets/images/logo.png';
+
+import styles from "./styles";
+import { useNavigation } from '@react-navigation/native';
+
+interface PageHeaderPropos {
+  title: string;
+  headerRight?: ReactNode;
+}
+
+const PageHeader: React.FC<PageHeaderPropos> = ({ title, headerRight, children }) => {
+
+  const { navigate } = useNavigation();
+
+  function handleGoBack() {
+    navigate('Landing');
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topBar}>
+        <BorderlessButton onPress={handleGoBack}>
+          <Image source={backIcon} resizeMode='contain'/>
+        </BorderlessButton>
+        <Image source={logoImage} resizeMode='contain' />
+      </View>
+
+      <View style={styles.header}>
+        <Text style={styles.title}>{title}</Text>
+        {headerRight}
+      </View>
+
+
+      {children}
+    </View>
+  );
+}
+
+export default PageHeader;
---
+++ mobile/src/pages/Favorites/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F0F0F7",
+  },
+
+  teacherList: {
+    marginTop: -20,
+  },
+
+});
+
+export default styles;
